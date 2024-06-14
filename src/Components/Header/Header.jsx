132d9e089154a431ef9f6fd8dc23faf9251b7939
Gean_Vitor <103613296+GeanVitor@users.noreply.github.com>
2024-06-14 14:20:30 -0300

    Adicionando resposividade

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,21 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { HiOutlineMenu, HiX } from 'react-icons/hi';
 import Icone from '../../Images/Icone_Fundo_Transparente.png';
 
 const Header = ({ bgColor, textColor, linkContact }) => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
     return (
-        <div className={`fixed top-0 left-0 w-full ${bgColor} z-10 shadow-lg`}>
-            <div className={`flex justify-between items-center px-16 py-6 ${textColor}`}>
+        <header className={`fixed top-0 left-0 w-full ${bgColor} z-10 shadow-lg`}>
+            <div className={`flex justify-between items-center px-4 md:px-16 py-4 md:py-6 ${textColor}`}>
                 <div className="flex items-center">
-                    <a href="/"><img src={Icone} alt="Logo" className="w-12 h-18" /></a>
-                    <div className={`mx-4 border-l-2 h-12 ${textColor}`}></div>
+                    <a href="/"><img src={Icone} alt="Logo" className="w-10 h-10 md:w-12 md:h-12" /></a>
+                    <div className={`hidden md:block mx-4 border-l-2 h-8 md:h-12 ${textColor}`}></div>
                     <a href="/">
-                        <h1 className="text-xl font-bold tracking-wide" style={{ fontFamily: 'Tomorrow, sans-serif' }}>
+                        <h1 className="text-lg md:text-xl font-bold tracking-wide ml-2" style={{ fontFamily: 'Tomorrow, sans-serif' }}>
                             Breach Busters
                         </h1>
                     </a>
                 </div>
-                <div>
-                    <ul className="flex space-x-10 font-bold " style={{ fontFamily: 'Anonymous Pro , sans-serif' }}>
+                <nav className="hidden md:block">
+                    <ul className="flex space-x-4 md:space-x-10 font-bold" style={{ fontFamily: 'Anonymous Pro, sans-serif' }}>
                         <li className="group">
                             <a href="/About" className="hover:text-gray-200 relative">
                                 Sobre
@@ -34,16 +37,55 @@ const Header = ({ bgColor, textColor, linkContact }) => {
                                 <span className={`block h-0.5 absolute left-0 bottom-0 w-0 group-hover:w-full transition-all duration-200 ${textColor}`}></span>
                             </a>
                         </li>
-                        <li className="group" style={{ fontFamily: 'Tomorrow  , sans-serif' }}>
-                            <a href={linkContact} className="bg-gradient-to-r from-purple-400 to-blue-400 hover:from-purple-600 hover:to-blue-600 text-white font-bold py-2 px-8 rounded-full relative">
+                        <li className="group" style={{ fontFamily: 'Tomorrow, sans-serif' }}>
+                            <a href={linkContact} className="bg-gradient-to-r from-purple-400 to-blue-400 hover:from-purple-600 hover:to-blue-600 text-white font-bold py-2 px-4 md:px-8 rounded-full relative">
                                 Contact Us
                             </a>
                         </li>
                     </ul>
+                </nav>
+                <div className="block md:hidden">
+                    <button onClick={() => setMenuOpen(!menuOpen)} aria-label="Toggle menu" className="focus:outline-none">
+                        {menuOpen ? <HiX className="w-8 h-8" /> : <HiOutlineMenu className="w-8 h-8" />}
+                    </button>
                 </div>
             </div>
-        </div>
+            {/* Drawer para dispositivos móveis */}
+            <div className={`fixed top-0 right-0 h-full w-full md:w-64 ${bgColor} shadow-lg transform ${menuOpen ? 'translate-x-0' : 'translate-x-full'} transition-transform duration-300 ease-in-out md:hidden z-20`}>
+                <div className="flex justify-end p-4">
+                    <button onClick={() => setMenuOpen(false)} aria-label="Close menu" className="focus:outline-none">
+                        <HiX className="w-8 h-8 text-white" />
+                    </button>
+                </div>
+                <nav className={`flex flex-col items-center py-4 text-white`}>
+                    <ul className="flex flex-col space-y-4 text-center font-bold" style={{ fontFamily: 'Anonymous Pro, sans-serif' }}>
+                        <li>
+                            <a href="/About" className="hover:text-gray-300">
+                                Sobre
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#blog" className="hover:text-gray-300">
+                                Blog
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#solucoes" className="hover:text-gray-300">
+                                Soluções
+                            </a>
+                        </li>
+                        <li>
+                            <a href={linkContact} className="bg-gradient-to-r from-purple-400 to-blue-400 hover:from-purple-600 hover:to-blue-600 text-white font-bold py-2 px-4 rounded-full">
+                                Contact Us
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            {/* Fundo transparente para clicar e fechar o menu */}
+            {menuOpen && <div className="fixed inset-0 bg-black opacity-50 z-10 md:hidden" onClick={() => setMenuOpen(false)}></div>}
+        </header>
     );
-}
+};
 
 export default Header;
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import illustration from '../../Images/Security On-bro.png'; 
+import illustration from '../../Images/Security On-bro.png';
 
-const Home = ({ bgColor , title, subTitle, text }) => {
+const Home = ({ bgColor, title, subTitle, text }) => {
   return (
-    <div className={`flex items-center justify-center min-h-screen ${bgColor} text-white`} style={{ fontFamily: 'Anonymous Pro  , sans-serif' }}>
+    <div className={`flex items-center justify-center min-h-screen ${bgColor} text-white font-sans`}>
       <div className="flex flex-col md:flex-row items-center justify-between max-w-6xl mx-auto px-4 py-12">
         <div className="md:w-1/2 text-center md:text-left">
           <h2 className="text-4xl md:text-5xl font-bold mt-20 mb-4">
@@ -12,14 +12,14 @@ const Home = ({ bgColor , title, subTitle, text }) => {
           <h2 className="text-4xl md:text-5xl font-bold mb-4 text-[#d11c9b]">
             {subTitle}
           </h2>
-          <p className="text-lg mb-8" style={{ fontFamily: 'Montserrat    , sans-serif' }}>
-           {text}
+          <p className="text-lg mb-8">
+            {text}
           </p>
-          <a href="#contact" style={{ fontFamily: 'Tomorrow   , sans-serif' }} className="bg-gradient-to-r from-purple-400 to-blue-400 hover:from-purple-600 hover:to-blue-600 text-white font-bold py-2 px-8 rounded-full">
+          <a href="#contact" className="bg-gradient-to-r from-purple-400 to-blue-400 hover:from-purple-600 hover:to-blue-600 text-white font-bold py-2 px-8 rounded-full">
             BECOME BREACHLESS
           </a>
         </div>
-        <div className="md:w-1/2 flex flex-col items-center justify-center mt-8 md:mt-0">
+        <div className="md:w-1/2 flex items-center justify-center mt-8 md:mt-0">
           <img src={illustration} alt="Illustration" className="w-2/3 md:w-full" />
         </div>
       </div>
