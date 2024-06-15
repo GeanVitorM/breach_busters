8cd50c1bb2f6e0c4a2c06cb0ff36f3027e39fbd0
Gean_Vitor <103613296+GeanVitor@users.noreply.github.com>
2024-06-15 10:55:04 -0300

    Adicionando icone a barra de navegacao do navegador.

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -6,12 +6,11 @@ const Footer = () => {
   return (
     <footer className="bg-black text-white py-8">
       <div className="container mx-auto px-4 md:px-6 lg:px-8">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
-          {/* Logo and Contact Info */}
+        <div className="flex flex-col md:flex-row justify-between items-center">
           <div className="flex flex-col items-center md:items-start">
             <div className="flex flex-row items-center space-x-4">
               <img src={skeletonLogo} alt="Logo" className="w-16 h-20 mb-4 md:mb-0" />
-              <h1 className="font-bold text-center md:text-left" style={{ fontFamily: 'Tomorrow, sans-serif' }}>Breach Busters</h1>
+              <h1 className="font-bold" style={{ fontFamily: 'Tomorrow, sans-serif' }}>Breach Busters</h1>
             </div>
             <div className="mt-4 text-center md:text-left" style={{ fontFamily: 'Montserrat, sans-serif' }}>
               <h3 className="text-[#d11c9b] font-bold">Whatsapp</h3>
@@ -20,25 +19,23 @@ const Footer = () => {
               <p className="text-lg font-bold">hello@reallygreatsite.com</p>
             </div>
           </div>
-
-          {/* Links Section */}
-          <div className="grid grid-cols-2 md:grid-cols-1 gap-4">
-            <div>
-              <h4 className="font-bold text-[#d11c9b] text-center md:text-left">Sobre</h4>
-              <ul className="text-center md:text-left">
+          <div className="flex flex-wrap justify-center md:justify-start mt-8 md:mt-0 font-sans" style={{ fontFamily: 'Anonymous Pro, sans-serif' }}>
+            <div className="mx-4">
+              <h4 className="font-bold text-[#d11c9b]">Sobre</h4>
+              <ul>
                 <li><a href="/About" className="hover:underline">About</a></li>
               </ul>
             </div>
-            <div>
-              <h4 className="font-bold text-[#d11c9b] text-center md:text-left">Blog</h4>
-              <ul className="text-center md:text-left">
+            <div className="mx-4">
+              <h4 className="font-bold text-[#d11c9b]">Blog</h4>
+              <ul>
                 <li>News</li>
                 <li>Tips</li>
               </ul>
             </div>
-            <div>
-              <h4 className="font-bold text-[#d11c9b] text-center md:text-left">Soluções</h4>
-              <ul className="text-center md:text-left">
+            <div className="mx-4">
+              <h4 className="font-bold text-[#d11c9b]">Soluções</h4>
+              <ul>
                 <li><a href="/Solutions/teste-de-intrusao" className="hover:underline">Teste de Intrusão</a></li>
                 <li><a href="/Solutions/Red-Team" className="hover:underline">Red Team</a></li>
                 <li><a href="/Solutions/treinamentos" className="hover:underline">Treinamentos</a></li>
@@ -47,30 +44,28 @@ const Footer = () => {
                 <li><a href="/Solutions/Engenharia-reversa" className="hover:underline">Engenharia reversa</a></li>
               </ul>
             </div>
-            <div>
-              <h4 className="font-bold text-[#d11c9b] text-center md:text-left">Hire Us</h4>
-              <ul className="text-center md:text-left">
+            <div className="mx-4">
+              <h4 className="font-bold text-[#d11c9b]">Hire Us</h4>
+              <ul>
                 <li>Work with us</li>
               </ul>
             </div>
           </div>
-
-          {/* Social Icons and Legal */}
-          <div className="flex flex-col items-center md:items-start">
-            <div className="flex space-x-4 mb-4">
-              <a href="#"><FaLinkedin className="text-xl cursor-pointer" /></a>
-              <a href="#"><FaYoutube className="text-xl cursor-pointer" /></a>
-              <a href="#"><FaInstagram className="text-xl cursor-pointer" /></a>
+        </div>
+        <div className="flex flex-col items-center md:items-start mt-8" style={{ fontFamily: 'Montserrat, sans-serif' }}>
+          <div className="flex space-x-4 mb-4 justify-center md:justify-start w-full">
+            <a href="#"><FaLinkedin className="text-xl cursor-pointer" /></a>
+            <a href="#"><FaYoutube className="text-xl cursor-pointer" /></a>
+            <a href="#"><FaInstagram className="text-xl cursor-pointer" /></a>
+          </div>
+          <div className="w-full border-3 border-white-100 py-4 text-center text-sm text-gray-500 border-t-4 border-white mt-4 flex justify-center md:justify-between">
+            <div className="flex flex-wrap space-x-2">
+              <a href="/Policy" className="hover:underline">Política</a>
+              <a href="/Terms" className="hover:underline">Termos</a>
+              <a href="/Privacy" className="hover:underline">Privacidade</a>
+              <a href="#!" className="hover:underline">Segurança</a>
             </div>
-            <div className="text-center md:text-left">
-              <div className="flex flex-wrap space-x-2 mb-2">
-                <a href="/Policy" className="hover:underline">Política</a>
-                <a href="/Terms" className="hover:underline">Termos</a>
-                <a href="/Privacy" className="hover:underline">Privacidade</a>
-                <a href="#!" className="hover:underline">Segurança</a>
-              </div>
-              <p className="text-sm text-gray-500">&copy; Breach Buster todos os direitos reservados</p>
-            </div>
+            <p>&copy; Breach buster todos os direitos reservados</p>
           </div>
         </div>
       </div>
