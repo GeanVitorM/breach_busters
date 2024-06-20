e6f2a318f5ba6a5ad26da797ccad99f404a1bf4e
Gean_Vitor <103613296+GeanVitor@users.noreply.github.com>
2024-06-20 08:48:04 -0300

    Retirando referencias unitilizadas.

--- src/Components/Benefits/Benefits.jsx
+++ src/Components/Benefits/Benefits.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect} from 'react';
 import Relogio from "../../Images/relogio.webp";
 
 const Benefits = () => {
